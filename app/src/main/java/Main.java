8a8da259332b437c7f766bcf607f24dc0ca4ba4b
Agnieszka Trzcińska <74868709+agabylicka@users.noreply.github.com>
2023-12-14 16:47:49 +0100

    Update Main.java
    Reverted changes.

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,76 +1,38 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        start();
-    }
-
-    public static void start() {
+        
+        Cw_2 srednia = new Cw_2();
+        srednia.average();
+        
+        Cw_3 obiekt = new Cw_3();
+        obiekt.three();
+        
+        Cw_4 trojkat = new Cw_4();
+        trojkat.four();
+        
+        Cw_5 cwiczenie = new Cw_5();
+        cwiczenie.five();
+        
+        Cw_6 suma = new Cw_6();
+        suma.count();
+        
+        Cw_7 fibonacci = new Cw_7();
+        fibonacci.seven();
+        
+        Cw_8 liczenie = new Cw_8();
+        liczenie.factorion();
+        
         Scanner scanner = new Scanner(System.in);
-        String input;
-        do {
-            System.out.println("Choose Your operation: add, subtract, divide or multiply. If You want quit, please write: exit ");
-            input = scanner.nextLine();
-            if (input.equals("add")) {
-                System.out.println("Give Your first number:");
-                int first = Integer.parseInt(scanner.nextLine());
-                System.out.println("Give Your second number:");
-                int second = Integer.parseInt(scanner.nextLine());
-                int result = add(first, second);
-                System.out.println(result);
-            } else if (input.equals("subtract")) {
-                System.out.println("Give Your first number:");
-                int first = Integer.parseInt(scanner.nextLine());
-                System.out.println("Give Your second number:");
-                int second = Integer.parseInt(scanner.nextLine());
-                int result = subtract(first, second);
-                System.out.println(result);
-            } else if (input.equals("divide")) {
-                System.out.println("Give Your first number:");
-                int first = Integer.parseInt(scanner.nextLine());
-                System.out.println("Give Your second number:");
-                int second = Integer.parseInt(scanner.nextLine());
-                int result = divide(first, second);
-                System.out.println(result);
-
-            } else if (input.equals("multiply")) {
-                System.out.println("Give Your first number:");
-                int first = Integer.parseInt(scanner.nextLine());
-                System.out.println("Give Your second number:");
-                int second = Integer.parseInt(scanner.nextLine());
-                int result = multiply(first, second);
-                System.out.println(result);
-
-            } else if (input.equals("exit")) {
-                System.exit(0);
-
-            } else {
-                System.out.println("Give proper answer");
-            }
-        } while (input.equals("add") || input.equals("subtract") || input.equals("divide") || input.equals("multiply"));
-    }
-
-    private static int add(int a, int b) {
-        return a + b;
-    }
-
-    private static int subtract(int a, int b) {
-        return a - b;
-    }
-
-    private static int divide(int a, int b) {
-        if ((a != 0) && (b != 0)) {
-            return a / b;
-        } else {
-            System.out.println("It is not possible divide by 0");
-            System.exit(0);
+        System.out.println("Podaj liczbe:");
+        String input = scanner.nextLine();
+        Integer number = Integer.parseInt(input);
+        if (number > 0) {
+            System.out.println("Positive");
+        }else{
+            System.out.println("Negative");
         }
-        return 0;
-    }
-
-    private static int multiply(int a, int b) {
-        return a * b;
+        
     }
 }
